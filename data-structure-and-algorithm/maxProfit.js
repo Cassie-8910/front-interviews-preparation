51478cbfa9b3a69f79bdb2b2063a2bd00acee6d2
cassie <cassie8910@163.com>
2021-01-09 21:20:32 +0800

    feat <algorithm>: leetcode daily problem

---
+++ data-structure-and-algorithm/maxProfit.js
@@ -0,0 +1,21 @@
+/**
+ * 买卖股票的最佳时机 III
+ * 动态规划
+ * @param {number[]} prices
+ * @return {number}
+ * 给定一个数组，它的第 i 个元素是一支给定的股票在第 i 天的价格。
+ * 设计一个算法来计算你所能获取的最大利润。你最多可以完成两笔交易。
+ * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+ */
+var maxProfit = function(prices) {
+    const n = prices.length;
+    let buy1 = -prices[0], buy2 = -prices[0];
+    let sell1 = 0, sell2 = 0;
+    for (let i = 1; i < n; i++) {
+        buy1 = Math.max(buy1, -prices[i]);
+        sell1 = Math.max(sell1, buy1 + prices[i]);
+        buy2 = Math.max(buy2, sell1 - prices[i]);
+        sell2 = Math.max(sell2, buy2 + prices[i]);
+    }
+    return sell2;
+};
